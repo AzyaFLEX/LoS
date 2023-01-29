1e64f812a1c26cfae87135a6cd899af5ab2b97cb
Азя <azyadr@yandex.ru>
2023-01-30 00:01:49 +0300

    version 0.5f: removed news models from news.py

--- db/models.py
+++ db/models.py
@@ -2,10 +2,9 @@ import datetime
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy import Column, String, DateTime, func, ForeignKey, Boolean
+from sqlalchemy import Column, String, DateTime, func, ForeignKey
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import relationship, declarative_mixin
-from sqlalchemy.sql import expression
 
 from db import Base
 from db.engine import get_async_session
@@ -30,52 +29,6 @@ class User(TimestampMixin, SQLAlchemyBaseUserTable, Base):
 
     first_name = Column(String(30), nullable=False)
     second_name = Column(String(30), nullable=False)
-
-    news = relationship('News', back_populates='author', order_by='News.created_at.desc()')
-    comments = relationship('Comment', back_populates='author', order_by='Comment.created_at.desc()')
-
-
-class News(TimestampMixin, Base):
-    __tablename__ = 'news'
-
-    title = Column(String(64), nullable=False)
-    context = Column(String(2048), nullable=False)
-    author_id = Column(ForeignKey('user.id'), nullable=False, unique=False)
-
-    comments = relationship('Comment', lazy='selectin')
-    rating = relationship('NewsRating', lazy='selectin')
-
-    author = relationship('User', back_populates='news', uselist=False)
-    file = relationship('NewsFile', back_populates='news', uselist=False)
-
-
-class Comment(TimestampMixin, Base):
-    __tablename__ = 'comment'
-
-    news_id = Column(ForeignKey('news.id'), nullable=False)
-    context = Column(String(1024), nullable=False)
-    author_id = Column(ForeignKey('user.id'), nullable=False)
-
-    author = relationship('User', back_populates='comments')
-
-
-class NewsRating(Base):
-    __tablename__ = 'news_rating'
-
-    user_id = Column(ForeignKey('user.id'), index=True, nullable=False)
-    news_id = Column(ForeignKey('news.id'), index=True, nullable=False)
-
-    positive = Column(Boolean, server_default=expression.true(), nullable=False)
-
-
-class NewsFile(Base):
-    __tablename__ = 'news_file'
-
-    news_id = Column(ForeignKey('news.id'), index=True, nullable=False, unique=True)
-    file_format = Column(String(10), nullable=False)
-    file_path = Column(String(1024), nullable=False)
-
-    news = relationship('News', back_populates='file')
 
 
 class CodeCharacter(Base):
